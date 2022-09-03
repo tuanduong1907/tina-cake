6f86a28b7768abdf142b54e94c96806427a343b9
tuanduong19077 <91052899+tuanduong1907@users.noreply.github.com>
2022-09-03 18:17:46 +0700

    update code

--- data/introImageListData.js
+++ data/introImageListData.js
@@ -20,3 +20,4 @@ export const introImageListData = [
       "https://images.unsplash.com/photo-1519733949-b7dfe6268abe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
   },
 ];
+
