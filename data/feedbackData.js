5cc383795838c7f6270d7ca67d35876a7b32b225
tuanduong19077 <91052899+tuanduong1907@users.noreply.github.com>
2022-09-05 16:41:00 +0700

    update code

--- data/feedbackData.js
+++ data/feedbackData.js
@@ -2,7 +2,7 @@ export const feedbackData = [
   {
     id: 1,
     image:
-      "https://lh3.googleusercontent.com/a-/AFdZucrK585I4dg1r8U5vxvtvkOhdChDGr7fAd2vMR4i=w60-h60-p-rp-mo-br100",
+      "https://firebasestorage.googleapis.com/v0/b/monkey-backend.appspot.com/o/images%2Fuser-feedback-1.png?alt=media&token=654ce283-08dd-41d4-9e4c-eb9e12a127fe",
     username: "Chung Lương",
     content:
       "Bánh của tiệm rất ngon , làm chuẩn mẫu lun . Lần nào sinh nhật mình cũng đặt bánh ở tiệm , tư vấn rất nhiệt tình và chu đáo",
@@ -10,7 +10,7 @@ export const feedbackData = [
   {
     id: 2,
     image:
-      "https://lh3.googleusercontent.com/a-/AFdZucoQJsnjQzUmyzsrSFmdG85ZAIIywJ8q5de1ujEc=w60-h60-p-rp-mo-ba3-br100",
+      "https://firebasestorage.googleapis.com/v0/b/monkey-backend.appspot.com/o/images%2Fuser-feedback-2.png?alt=media&token=f0496674-8b7a-4b5f-b2a4-296361e17d7c",
     username: "Đào Trần Thị",
     content:
       "Được một người bạn giới thiệu tiệm bánh này rất ưng luôn. Bánh đẹp, mềm mịn và không quá ngọt, rất vừa ăn. Mình sẽ ủng hộ lâu dài.",
@@ -18,7 +18,7 @@ export const feedbackData = [
   {
     id: 3,
     image:
-      "https://lh3.googleusercontent.com/a-/AFdZucrJCyLKctCJwx-faLPZvnfmGTBsrVydrH9Z01vv=w60-h60-p-rp-mo-br100",
+      "https://firebasestorage.googleapis.com/v0/b/monkey-backend.appspot.com/o/images%2Fuser-feedback-3.png?alt=media&token=1627394d-a0ad-4924-a965-38cf0af15d53",
     username: "Trang Trang",
     content:
       "Được một người bạn giới thiệu tiệm bánh này rất ưng luôn. Bánh đẹp, mềm mịn và không quá ngọt, rất vừa ăn. Mình sẽ ủng hộ lâu dài",
@@ -26,7 +26,7 @@ export const feedbackData = [
   {
     id: 4,
     image:
-      "https://lh3.googleusercontent.com/a-/AFdZucrzK5Zc-z-jC7-uFU6ckKiqmg8zjE_o0f5TijyYkQ=w60-h60-p-rp-mo-ba3-br100",
+      "https://firebasestorage.googleapis.com/v0/b/monkey-backend.appspot.com/o/images%2Fuser-feedback-5.png?alt=media&token=b979b16f-54ca-4c99-ad28-f478f25f4e8d",
     username: "Phong Trần Trai",
     content:
       "Tiệm bánh sinh nhật tuyệt vời nhất ❤️❤️❤️ Chất lượng bánh quá tuyệt, phục vụ chu đáo, tư vấn nhiệt tình ❤️ Sẽ ủng hộ dài dài và giới thiệu cho người thân bạn bè ủng hộ😍😍😍",
@@ -34,7 +34,7 @@ export const feedbackData = [
   {
     id: 5,
     image:
-      "https://lh3.googleusercontent.com/a-/AFdZucqagCNLMg6N2o_BSC3LyrqFPeCvL-AS1dqsFuLKww=w60-h60-p-rp-mo-br100",
+      "https://firebasestorage.googleapis.com/v0/b/monkey-backend.appspot.com/o/images%2Fuser-feedback-4.png?alt=media&token=c5813da6-1501-418c-96e3-4600bf9ca17a",
     username: "Hồng Quân Mạc",
     content:
       "Bánh siêu thơm ngon, đẹp, không bị chảy sệ như mấy bánh ở tiệm khác. Tiệm có nhân viên trực ib siêu nhiệt tình và chị call xác nhận giọng siêu đáng yêu",
