02993c923509c569a05851464e04b1484b9e2d6f
tuanduong19077 <91052899+tuanduong1907@users.noreply.github.com>
2022-09-15 00:16:40 +0700

    update code

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,14 +1,27 @@
+import { signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useAuth } from "../../project/contexts/auth-context";
+import AppButton from "../../project/controls/app-button/AppButton";
+import { auth } from "../../project/firebase/firebase-config";
 
 const Index = () => {
   const router = useRouter();
   const { userInfo } = useAuth();
+
   useEffect(() => {
     if (!userInfo) router.push("/dang-nhap");
   }, [router, userInfo]);
-  return <div>Admin</div>;
+
+  const handleSignOut = () => {
+    signOut(auth);
+  };
+
+  return (
+    <div>
+      <AppButton onClick={handleSignOut}>Đăng xuất</AppButton>
+    </div>
+  );
 };
 
 export default Index;
