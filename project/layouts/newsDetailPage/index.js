fe46b86707ea9d921b81802d719411a39e9dd769
tuanduong19077 <91052899+tuanduong1907@users.noreply.github.com>
2022-10-16 22:07:49 +0700

    update code

--- project/layouts/newsDetailPage/index.js
+++ project/layouts/newsDetailPage/index.js
@@ -108,11 +108,16 @@ const NewsDetailPage = ({ slug }) => {
   return (
     <>
       <HeadSeo
-        image={postInfo?.image}
+        image={
+          postInfo?.image ||
+          "https://firebasestorage.googleapis.com/v0/b/backend-tina-cake.appspot.com/o/avatar.jpg?alt=med ia&token=93c15490-efec-4d4d-9574-0f0cf008d76a"
+        }
         description={postInfo.desc}
-        key={postInfo?.title}
+        key={
+          postInfo?.title ||
+          "Bánh Kem Nha Trang Tina là tiệm bánh sinh nhật - bánh cưới Nha Trang chuyên bán sẵn, nhận đặt, làm tươi và giao nhanh các loại bánh kem sinh nhật, thôi nôi. Nhận đặt các loại bánh Âu sinh nhật, bánh kem sinh nhật fondant và cupcake tại Nha Trang"
+        }
         title={postInfo?.title || "Không tìm thấy bài viết | Tina Cake"}
-        og={propInfo?.title}
       />
       <NewsDetailPageStyles className="container py-layout">
         <div className="news-detail-content entry-content">
