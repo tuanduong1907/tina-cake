fac551d52f8a465ebcc90bd5b4f25b1ecc5ef24d
tuanduong19077 <91052899+tuanduong1907@users.noreply.github.com>
2022-10-16 22:37:42 +0700

    update code

--- project/components/SEO/index.js
+++ project/components/SEO/index.js
@@ -2,7 +2,12 @@ import Head from 'next/head';
 const HeadSeo = (props) => {
     return (
         <Head>
-
+            <title>{props.title || "Trang chủ | Tina Cake"}</title>
+            <meta property="og:title" content={props.title || "Trang chủ | Tina Cake"} />
+            <meta property="og:description" content={props.description} />
+            <meta property="og:type" content="article" />
+            <meta property="og:url" content="https://tina-cake.vercel.app/" />
+            <meta property="og:image" content={props.image || 'https://firebasestorage.googleapis.com/v0/b/backend-tina-cake.appspot.com/o/avatar.jpg?alt=media&token=93c15490-efec-4d4d-9574-0f0cf008d76a'}/>
         </Head>
     )
 }
