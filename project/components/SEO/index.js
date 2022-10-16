08ef1f356597b15809cc816850fdb9f9291c1042
tuanduong19077 <91052899+tuanduong1907@users.noreply.github.com>
2022-10-16 22:58:15 +0700

    update copde

--- project/components/SEO/index.js
+++ project/components/SEO/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 const HeadSeo = (props) => {
   return (
     <Head>
-      <title>{props.title || "Trang chủ | Tina Cake"}</title>
+      {/* <title>{props.title || "Trang chủ | Tina Cake"}</title>
       <meta name="keywords" content="your, tags" />
       <meta name="description" content={props.description} />
       <meta name="copyright" content="Tina Cake" />
@@ -28,7 +28,18 @@ const HeadSeo = (props) => {
           props.image ||
           "https://firebasestorage.googleapis.com/v0/b/backend-tina-cake.appspot.com/o/avatar.jpg?alt=media&token=93c15490-efec-4d4d-9574-0f0cf008d76a"
         }
-      />
+      /> */}
+    <title>Bánh Kem Nha Trang Tina Cake - Bánh sinh Nhật Nha Trang - Bánh fondant - Cupcake</title>
+    <meta data-n-head="ssr" charset="utf-8" />
+    <meta data-n-head="ssr" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0" />
+    <meta data-n-head="ssr" name="sso_key" content="customers" />
+    <meta data-n-head="ssr" name="keywords" content="Bánh Kem Nha Trang Tina Cake - Bánh sinh Nhật Nha Trang - Bánh fondant - Cupcake  – Cửa hàng bánh Sinh Nhật – Bánh Cưới chuyên bán sẵn, nhận đặt, làm tươi và giao nhanh các loại bánh kem sinh nhật, thôi nôi trong vòng 1-8 giờ nhận đơn. Mẫu mã hiện đại, đáp ứng nhu cầu của những khách hàng cần bánh gấp, đòi hỏi cao về chất lượng bánh và kem (mới ra lò), đặc biệt là khách du lịch ở Nha Trang." />
+    <meta data-n-head="ssr" name="description" content="Bánh Kem Nha Trang Tina Cake - Bánh sinh Nhật Nha Trang - Bánh fondant - Cupcake  – Cửa hàng bánh Sinh Nhật – Bánh Cưới chuyên bán sẵn, nhận đặt, làm tươi và giao nhanh các loại bánh kem sinh nhật, thôi nôi trong vòng 1-8 giờ nhận đơn. Mẫu mã hiện đại, đáp ứng nhu cầu của những khách hàng cần bánh gấp, đòi hỏi cao về chất lượng bánh và kem (mới ra lò), đặc biệt là khách du lịch ở Nha Trang." />
+    <meta data-n-head="ssr" property="og:url" content="https://mykiotfe" />
+    <meta data-n-head="ssr" property="og:type" content="website" />
+    <meta data-n-head="ssr" property="og:title" content="Bánh Kem Nha Trang Tina Cake - Bánh sinh Nhật Nha Trang - Bánh fondant - Cupcake" />
+    <meta data-n-head="ssr" property="og:description" content="Bánh Kem Nha Trang Tina Cake - Bánh sinh Nhật Nha Trang - Bánh fondant - Cupcake  – Cửa hàng bánh Sinh Nhật – Bánh Cưới chuyên bán sẵn, nhận đặt, làm tươi và giao nhanh các loại bánh kem sinh nhật, thôi nôi trong vòng 1-8 giờ nhận đơn. Mẫu mã hiện đại, đáp ứng nhu cầu của những khách hàng cần bánh gấp, đòi hỏi cao về chất lượng bánh và kem (mới ra lò), đặc biệt là khách du lịch ở Nha Trang." />
+    <meta data-n-head="ssr" property="og:image" content="https://d3rzzb9pdm93i0.cloudfront.net/2021/12/1639409961f24e427f68f7ff71812edee6d360e9a4.png" />
     </Head>
   );
 };
